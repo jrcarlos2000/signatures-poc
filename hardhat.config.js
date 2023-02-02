d0c0104859f55d837284782b4b3cc94316fe8a77
CarlosR <carlos.ramos@grupoimportia.com>
2023-02-02 11:51:01 +0800

    fix : commented networks for easy test

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,8 +17,8 @@ const mnemonic =
 
 let privateKeys = [];
 
-//creates the list of private keys from the mnemonic
-let derivePath = "m/44'/60'/0'/0/";
+// creates the list of private keys from the mnemonic
+const derivePath = "m/44'/60'/0'/0/";
 for (let i = 0; i <= 10; i++) {
   const wallet = new ethers.Wallet.fromMnemonic(mnemonic, `${derivePath}${i}`);
   privateKeys.push(wallet.privateKey);
@@ -35,9 +35,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 task("fund", "Fund accounts on fork")
   .addOptionalParam("amount", "Stable coin amount to fund each account with")
   .addOptionalParam("fundnetwork", "forked Network")
-  .addOptionalParam("localaccounts"," number of first local accounts to fund")
+  .addOptionalParam("localaccounts", " number of first local accounts to fund")
   .setAction(fund);
-
 
 module.exports = {
   solidity: {
@@ -64,63 +63,63 @@ module.exports = {
       chainId: 1337,
       timeout: 6000000,
     },
-    mainnet : {
-      url : `${process.env.MAINNET_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 1,
-    },
-    skale : {
-      url : `${process.env.SKALE_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 0xafcee83030b95,
-    },
-    polygonMumbai: {
-      url: `${process.env.MUMBAI_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 80001,
-    },
-    polygon: {
-      url: `${process.env.POLYGON_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 137,
-    },
-    bscTestnet: {
-      url: `${process.env.BSCTEST_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 97,
-    },
-    bsc: {
-      url: `${process.env.BSC_PROVIDER_URL}`,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
-        process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
-      ],
-      chainId: 56,
-    }
+    // mainnet : {
+    //   url : `${process.env.MAINNET_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 1,
+    // },
+    // skale : {
+    //   url : `${process.env.SKALE_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 0xafcee83030b95,
+    // },
+    // polygonMumbai: {
+    //   url: `${process.env.MUMBAI_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 80001,
+    // },
+    // polygon: {
+    //   url: `${process.env.POLYGON_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 137,
+    // },
+    // bscTestnet: {
+    //   url: `${process.env.BSCTEST_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 97,
+    // },
+    // bsc: {
+    //   url: `${process.env.BSC_PROVIDER_URL}`,
+    //   accounts: [
+    //     process.env.DEPLOYER_PRIVATE_KEY || privateKeys[2],
+    //     process.env.GOVERNOR_PRIVATE_KEY || privateKeys[1],
+    //   ],
+    //   chainId: 56,
+    // }
   },
   namedAccounts: {
     deployerAddr: {
       default: 0,
-      localhost: 0
+      localhost: 0,
     },
     governorAddr: {
       default: 1,
-      localhost: 1
+      localhost: 1,
     },
   },
   gasReporter: {
@@ -135,7 +134,7 @@ module.exports = {
     apiKey: {
       polygonMumbai: process.env.POLYGONSCAN_API_KEY,
       bscTestnet: process.env.BSCSCAN_API_KEY,
-      polygon : process.env.POLYGONSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
     },
   },
 };
